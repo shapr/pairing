ee4afc16ff75bac18f08b4eb340c6ca2a80ce924
Shae Erisson <shae@scannedinavian.com>
2022-03-29 12:57:32 -0400

    beginning of a parser

--- src/Pairing.hs
+++ src/Pairing.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Pairing where
+
+import Control.Applicative
+import Data.Attoparsec.ByteString.Char8
 
 pairing :: String
 pairing = "hello world"
@@ -8,9 +13,15 @@ pairing = "hello world"
 data Color = Red | Orange | Yellow | Green | Blue | Purple
   deriving (Show, Eq)
 
+-- R, O, Y, G, B, P
+colorP :: Parser Color
+colorP =
+  Red <$ string "R"
+    <|> Orange <$ string "O"
+
 -- this can use ReadP, or it could just check strings
-parse :: String -> Maybe Color
-parse = undefined
+-- parse :: String -> Maybe Color
+-- parse = undefined
 
 -- this might be awkward to check against a guess
 -- it would also be awkward (lots of small checks) to check against a list
